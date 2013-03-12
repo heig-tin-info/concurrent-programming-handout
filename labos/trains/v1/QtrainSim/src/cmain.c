1648ae2e09f63cc70d5ca484cb2862acff1f2dd9
Yann Sutter <yann.sutter@heig-vd.ch>
2013-03-12 14:12:44 +0100

    Clean up of the cmain code

--- labos/trains/v1/QtrainSim/src/cmain.c
+++ labos/trains/v1/QtrainSim/src/cmain.c
@@ -2,78 +2,83 @@
 #include "ctrain_handler.h"
 #include <errno.h>
 
-
-// structure qui definit une locomotive
+//Engine definition
 typedef struct
 {
-  int no;
-  int vitesse;
+    int no;
+    int vitesse;
 } Locomotive;
 
 
-// Declaration des deux locomotives
+//Engines declaration
 Locomotive loco1;
+Locomotive loco2;
 
+//Engines itineraries
+#define NB_CONTACTS 7
+int it1[] = {6, 11, 10, 13, 14, 19, 3};
+int it2[] = {6, 11, 10, 13, 14, 19, 3};
 
+//Emergency stop for the engines
 void emergency_stop()
 {
-  printf("\nSTOP!");
+    afficher_message("\nEMERGENCY STOP!!");
 
-  // on arrete les locomotives.
-  arreter_loco(loco1.no);
+    //Stop the engines
+    arreter_loco(loco1.no);
+    arreter_loco(loco2.no);
 }
 
-
-// Contacts a parcourir
-#define NB_CTS 7
-int parcours[] = {6, 11, 10, 13, 14, 19, 3};
-
-
+//Main function
 void cmain()
 {
-  int ct;
+    //Itinary counter
+    int ct;
 
-  loco1.no =1;
-  loco1.vitesse = 12;
+    //Engine 1 settings
+    loco1.no = 1;
+    loco1.vitesse = 12;
 
- // init_maquette();
-  selection_maquette("MAQUET_B");
+    //Engine 2 settings
+    loco2.no = 2;
+    loco2.vitesse = 12;
 
+    //Initializes the maquet
+    selection_maquette("MAQUET_B");
+    afficher_message("Et c'est parti!!");
 
-  afficher_message("Et c'est parti!!");
+    //Initializes engine 1 initial position
+    assigner_loco(11, 6, loco1.no, loco1.vitesse);
+    afficher_message_loco(loco1.no,"Hello, je suis la loco 1");
 
-  // Demande au simulateur de placer une loco entre les contacts 6 et 11
-  // Recupere le numero et la vitesse saisis par l'utilisateur.
-  assigner_loco(  11,
-                  6,
-                  loco1.no,
-                  loco1.vitesse);
+    //Initializes engine 2 initial position
+    assigner_loco(4, 9, loco2.no, loco2.vitesse);
+    afficher_message_loco(loco2.no,"Hello, je suis la loco 2");
 
-  afficher_message_loco(loco1.no,"Hello, je suis la loco 1");
-  // Dirige les aiguillages sur le parcours
-  diriger_aiguillage(7,TOUT_DROIT,0);
-  diriger_aiguillage(8,DEVIE,0);
-  diriger_aiguillage(5,TOUT_DROIT,0);
-  diriger_aiguillage(9,DEVIE,0);
-  diriger_aiguillage(10,TOUT_DROIT,0);
-  diriger_aiguillage(14,TOUT_DROIT,0);
-  diriger_aiguillage(13,DEVIE,0);
-  diriger_aiguillage(1,TOUT_DROIT,0);
+    //Initializes the points
+    diriger_aiguillage(7,TOUT_DROIT,0);
+    diriger_aiguillage(8,DEVIE,0);
+    diriger_aiguillage(5,TOUT_DROIT,0);
+    diriger_aiguillage(9,TOUT_DROIT,0);
+    diriger_aiguillage(10,TOUT_DROIT,0);
+    diriger_aiguillage(14,TOUT_DROIT,0);
+    diriger_aiguillage(13,TOUT_DROIT,0);
+    diriger_aiguillage(1,TOUT_DROIT,0);
 
+    //Start the engines
+    mettre_vitesse_progressive(loco1.no, loco1.vitesse);
+    mettre_vitesse_progressive(loco2.no, loco2.vitesse);
 
-  // Demarre la loco
-  mettre_vitesse_progressive(loco1.no, loco1.vitesse);
+    // Attend que la loco passe sur les differents contacts de son parcours.
+    for (ct = 1; ct < NB_CONTACTS; ct++) {
+        attendre_contact(it1[ct]);
+        printf("Loco %d de vitesse %d a atteint le contact %d.\n", loco1.no, loco1.vitesse, ct);
+    }
 
-  // Attend que la loco passe sur les differents contacts de son parcours.
-  for (ct = 1; ct < NB_CTS; ct++) {
-    attendre_contact(parcours[ct]);
-    printf("Loco %d de vitesse %d a atteint le contact %d.\n", loco1.no, loco1.vitesse, ct);
-  }
+    //Stop the engines
+    arreter_loco(loco1.no);
+    arreter_loco(loco2.no);
 
-  // Stoppe la loco
-  arreter_loco(loco1.no);
-
-  // Fin de la simulation (a effectuer une seule fois en fin de programme, sans effet
-  // sur le simulateur, mais necessaire sur les maquettes reelles).
-  mettre_maquette_hors_service();
+    //Shutdown the maquet
+    mettre_maquette_hors_service();
 }
