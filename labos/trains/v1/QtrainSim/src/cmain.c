41530e2363f674c753c73d1445ff582cf8008219
Jeremie Ecoffey <jeremie.ecoffey@heig-vd.ch>
2013-03-18 12:48:56 +0100

    quick fix

--- labos/trains/v1/QtrainSim/src/cmain.c
+++ labos/trains/v1/QtrainSim/src/cmain.c
@@ -61,9 +61,6 @@ void cmain()
   diriger_aiguillage(1,TOUT_DROIT,0);
 
 
-  const char* tmp = getCommand();
-  afficher_message(tmp);
-
   // Demarre la loco
   mettre_vitesse_progressive(loco1.no, loco1.vitesse);
 
